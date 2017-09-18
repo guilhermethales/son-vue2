c96b5d3e3d7287cfb3eac191e546aa2ae1421b05
Unknown <guilhermevtog@hotmail.com>
2017-09-18 14:19:11 -0300

    Iniciando projeto de estudo view.

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <p :id="numberInteger">{{ title }}</p>
+    <a :href="link">Teste</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'Este é um título teste',
+      numberInteger: 10,
+      link: 'http://localhost:8080'
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
