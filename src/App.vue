d02773dde35919addc56a391ca1d93dafb56a1e1
Unknown <guilhermevtog@hotmail.com>
2017-09-18 16:51:44 -0300

    Listando times da série A

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <ul>
-      <li v-for="team in teams">
-        {{ team.name }} <img :src="team.shield">
-      </li>
-      <li v-for="(value, key, index) in alfabeto">
-        {{ index }} - {{ key }} : {{ value }}
-      </li>
-    </ul>
+  <div id="app" class="container">
+    <div class="row">
+      <h3>Campeonato Brasileiro - Série A - 2016</h3>
+      <br><br>
+
+      <table class="table table-striped">
+        <thead>
+          <th>Time</th>
+          <th>Pontos</th>
+          <th>GF</th>
+          <th>GS</th>
+          <th>Saldo</th>
+        </thead>
+
+        <tbody>
+          <tr v-for="team in teams">
+            <td>
+              <img :src="team.shield" style="height: 30px; width: 30px;">
+              <strong>{{ team.name }}</strong>
+            </td>
+            <td>{{ team.points }}</td>
+            <td>{{ team.goalsScored }}</td>
+            <td>{{ team.goalsConceded }}</td>
+            <td>{{ team.goalsScored - team.goalsConceded }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -23,7 +41,23 @@ export default {
         new Team('América-MG', require('./assets/img/america_mg_60x60.png')),
         new Team('Atlético-MG', require('./assets/img/atletico_mg_60x60.png')),
         new Team('Atlético-PR', require('./assets/img/atletico-pr_60x60.png')),
-        new Team('Botafogo', require('./assets/img/botafogo_60x60.png'))
+        new Team('Botafogo', require('./assets/img/botafogo_60x60.png')),
+        new Team('Chapecoense', require('./assets/img/chapecoense_60x60.png')),
+        new Team('Corinthians', require('./assets/img/corinthians_60x60.png')),
+        new Team('Coritiba', require('./assets/img/coritiba_60x60.png')),
+        new Team('Cruzeiro', require('./assets/img/cruzeiro_60x60.png')),
+        new Team('Figueirense', require('./assets/img/figueirense_60x60.png')),
+        new Team('Flamengo', require('./assets/img/flamengo_60x60.png')),
+        new Team('Fluminense', require('./assets/img/fluminense_60x60.png')),
+        new Team('Gremio', require('./assets/img/gremio_60x60.png')),
+        new Team('Internacional', require('./assets/img/internacional_60x60.png')),
+        new Team('Palmeiras', require('./assets/img/palmeiras_60x60.png')),
+        new Team('Ponte Preta', require('./assets/img/ponte_preta_60x60.png')),
+        new Team('Santa Cruz', require('./assets/img/santa_cruz_60x60.png')),
+        new Team('Santos', require('./assets/img/santos_60x60.png')),
+        new Team('São Paulo', require('./assets/img/sao_paulo_60x60.png')),
+        new Team('Sport', require('./assets/img/sport_60x60.png')),
+        new Team('Vitória', require('./assets/img/vitoria_60x60.png'))
       ],
       alfabeto: {
         a: 'A',
